f1494cb3d86a106b931fc1fa4220fab694844df3
Adam <adamjsmith117@gmail.com>
2023-09-15 10:51:50 -0600

    add brex package

---
+++ packages/brex/src/types/common.ts
@@ -0,0 +1,34 @@
+import { pipe } from '@effect/data/Function'
+import * as S from '@effect/schema/Schema'
+
+/**
+ * PaymentType
+ */
+export const PaymentType = S.literal(
+  'ACH',
+  'BOOK_TRANSFER',
+  'CHEQUE',
+  'DOMESTIC_WIRE',
+  'INTERNATIONAL_WIRE'
+)
+export type PaymentType = S.To<typeof PaymentType>
+
+/**
+ * CurrencyAmount
+ */
+export const CurrencyAmount = pipe(
+  S.number,
+  S.int(),
+  S.nonNegative(),
+  S.description('Integer amount in lowest currency denomination')
+)
+export type CurrencyAmount = S.To<typeof CurrencyAmount>
+
+/**
+ * Money
+ */
+export const Money = S.struct({
+  amount: CurrencyAmount,
+  currency: S.nullable(S.literal('USD')), // ISO 4217 format. default: USD
+})
+export type Money = S.To<typeof Money>
