0dc9c8ad39a9a52473dc2eb0bf04c82e5e245baf
Adam <adamjsmith117@gmail.com>
2023-10-03 10:09:00 -0600

    getPresignedUrl

--- packages/gcp-gcs/src/index.ts
+++ packages/gcp-gcs/src/index.ts
@@ -15,6 +15,12 @@ type GCSWriteError = {
   stack: unknown
 }
 
+type GCSUrlSigningError = {
+  _tag: 'GCSUrlSigningError'
+  meassage: string
+  stack: unknown
+}
+
 /**
  * Writes data to key in bucket
  * @param bucket
@@ -31,6 +37,30 @@ export const write = (
       try: () => gcs.bucket(bucket).file(key).save(data),
       catch: (e) => ({
         _tag: 'GCSWriteError',
+        message: `${e}`,
+        error: (e as Error).stack,
+      }),
+    })
+  )
+
+export const getPresignedUrl = (
+  bucket: string,
+  key: string,
+  lifetime: number // time to live in milliseconds (relative to when URL is created)
+) =>
+  Effect.flatMap(GCS, (gcs) =>
+    Effect.tryPromise({
+      try: () =>
+        gcs
+          .bucket(bucket)
+          .file(key)
+          .getSignedUrl({
+            version: 'v4',
+            action: 'read',
+            expires: Date.now() + lifetime,
+          }),
+      catch: (e) => ({
+        _tag: 'GCSUrlSigningError',
         message: `${e}`,
         error: (e as Error).stack,
       }),
